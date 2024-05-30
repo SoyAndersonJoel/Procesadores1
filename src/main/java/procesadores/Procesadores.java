40ffcb5a79caa273f3cf79bb3c437ad167f4e8c2
SoyAndersonJoel <161794137+SoyAndersonJoel@users.noreply.github.com>
2024-05-30 08:19:07 -0500

    Class father

---
+++ src/main/java/procesadores/Procesadores.java
@@ -0,0 +1,52 @@
+package procesadores;
+
+public class Procesadores {
+    int nucleos;
+    double consumo;
+    String nombre;
+
+    public Procesadores() {
+    }
+
+    public Procesadores(int nucleos, double consumo, String nombre) {
+        this.nucleos = nucleos;
+        this.consumo = consumo;
+        this.nombre = nombre;
+    }
+
+    public int getNucleos() {
+        return nucleos;
+    }
+
+    public void setNucleos(int nucleos) {
+        this.nucleos = nucleos;
+    }
+
+    public double getConsumo() {
+        return consumo;
+    }
+
+    public void setConsumo(double consumo) {
+        this.consumo = consumo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+    //    Metodos Presonalizados
+
+
+    public void mostrar() {
+        System.out.println("Nucleos: " + nucleos);
+        System.out.println("Consumo: " + consumo);
+        System.out.println("Nombre: " + nombre);
+        System.out.println("");
+    }
+
+}

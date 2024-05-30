d0e37d7e347f4c394f17705fdb947a5b762f0d58
SoyAndersonJoel <161794137+SoyAndersonJoel@users.noreply.github.com>
2024-05-30 09:44:17 -0500

    Anderson Vilatuña

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,9 +6,10 @@ public class Main {
     public static void main(String[] args) {
 //
         Procesadores_AMD AMD1 = new Procesadores_AMD();
-        Procesadores_AMD AMD2 = new Procesadores_AMD(50,20.0,"DU","HIDL","si");
-        //Christian Márquez Integrante 1
-        Procesadores_AMD AMD3 = new Procesadores_AMD(8, 95.0, "Rysen 9 5900x",  "AM4", "60°C");
+        Procesadores_AMD AMD0 = new Procesadores_AMD(50,20.0,"DU","HIDL","si");
+//        Integrante 1
+        //Christian Márquez
+        Procesadores_AMD AMD2 = new Procesadores_AMD(8, 95.0, "Rysen 9 5900x",  "AM4", "60°C");
         Procesadores_Intel Intel1 = new Procesadores_Intel(6, 65.0, "Core i5-11600K", true);
         Procesadores_Moviles Moviles1 = new Procesadores_Moviles(8, 15.0, "Snapdragon 888", true);
 
@@ -20,6 +21,14 @@ public class Main {
         Procesadores_AMD AMD = new Procesadores_AMD(12, 150.0, "Ryzen 9 5900HX", "AM4", "70°C");
         Procesadores_Intel Intel = new Procesadores_Intel(8, 3.2, "Core i7", true);
         Procesadore_Escritorio esc = new Procesadore_Escritorio(8, 95.0, "Intel Core i7-11700K", "LGA 1200");
+        // Integrante 3
+        // Anderson Vilatuña
+        Procesadores_AMD AMD3 = new Procesadores_AMD(12, 150.0, "Ryzen 7 5900", "AM9", "79°C");
+        Procesadores_Intel Intel3 = new Procesadores_Intel(8, 3.2, "Core i8G", true);
+        Procesadore_Escritorio esc3 = new Procesadore_Escritorio(8, 95.0, " Core i7-6400", "LGA 200");
+
+
+
 
 //        integrante3
         Procesadores p1 = new Procesadores();

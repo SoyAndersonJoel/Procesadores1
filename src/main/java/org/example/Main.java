82991505a4238715601b7044fba3e443dad88870
SoyAndersonJoel <161794137+SoyAndersonJoel@users.noreply.github.com>
2024-05-30 08:45:06 -0500

    metodosAMD

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,16 @@
 package org.example;
 
 import procesadores.Procesadores;
+import procesadores.Procesadores_AMD;
 import procesadores.Procesadores_Moviles;
 
 public class Main {
     public static void main(String[] args) {
 //        integrante1
-        Pro
+        Procesadores_AMD AMD1 = new Procesadores_AMD();
+        Procesadores_AMD AMD2 = new Procesadores_AMD(50,20.0,"DU","HIDL","si");
+
+
 //        integrante2
 //        integrante3
         Procesadores p1 = new Procesadores();
--- src/main/java/procesadores/Procesadores_AMD.java
+++ src/main/java/procesadores/Procesadores_AMD.java
@@ -1,5 +1,23 @@
 package procesadores;
 
-public class Procesadores_AMD extends P {
+public class Procesadores_AMD extends  Procesadore_Escritorio{
+    String temperatura;
+    public Procesadores_AMD() {
+    }
 
+    public Procesadores_AMD(int nucleos, double consumo, String nombre, String socket,String temperatura) {
+        super(nucleos, consumo, nombre, socket);
+        this.temperatura = temperatura;
+    }
+
+    public String getTemperatura() {
+        return temperatura;
+    }
+
+    public void setTemperatura(String temperatura) {
+        this.temperatura = temperatura;
+    }
+    public void mostrarTemperatura() {
+        System.out.println("Temperatura: "+temperatura);
+    }
 }
